169e1ecae649ea4bfe05d873aac50364bba69bbd
jcoutch <joseph.coutcher@gmail.com>
2022-03-12 00:24:20 -0500

    More fixes

--- bt_light_control.sh
+++ bt_light_control.sh
@@ -28,9 +28,9 @@ hue_to_rgb() {
 
   [$vH -lt 0] && $((vh += 1))
   [$vH -gt 1] && $((vH -= 1))
-  if [(( 6 * vH )) -lt 1 ]; then return $(( v1 + ( v2 - v1 ) * 6 * vH )); fi
-  if [(( 2 * vH )) -lt 1 ]; then return $(( v2 )); fi
-  if [(( 3 * vH )) -lt 2 ]; then return $( v1 + ( v2 - v1 ) * ( ( 2 / 3 ) - vH ) * 6)); fi
+  if [$(( 6 * vH )) -lt 1 ]; then return $(( v1 + ( v2 - v1 ) * 6 * vH )); fi
+  if [$(( 2 * vH )) -lt 1 ]; then return $(( v2 )); fi
+  if [$(( 3 * vH )) -lt 2 ]; then return $(( v1 + ( v2 - v1 ) * ( ( 2 / 3 ) - vH ) * 6)); fi
 
   return $v1
 }
@@ -46,26 +46,26 @@ hsl_to_rgb () {
   local var_2=0
 
   if [$S -eq 0]; then
-    R = ((L * 255))
-    G = ((L * 255))
-    B = ((L * 255))
+    R = $((L * 255))
+    G = $((L * 255))
+    B = $((L * 255))
   else
-    if [(( L -lt 0.5 ))]; then
-      var_2 = ((L * ( 1 + S )))
+    if [$L -lt 0.5]; then
+      var_2 = $((L * ( 1 + S )))
     else
-      var_2 = ((( L + S ) - ( S * L )))
+      var_2 = $((( L + S ) - ( S * L )))
     fi
 
-    var_1 = ((2 * L - var_2))
+    var_1 = $((2 * L - var_2))
 
-    local temp_val = hue_to_rgb $var_1 $var_2 ((H + ( 1 / 3 )))
-    R = ((255 * temp_val))
+    local temp_val = hue_to_rgb $var_1 $var_2 $((H + ( 1 / 3 )))
+    R = $((255 * temp_val))
 
     temp_val = hue_to_rgb $var_1 $var_2 $H
-    G = ((255 * temp_val))
+    G = $((255 * temp_val))
 
-    temp_val = hue_to_rgb $var_1 $var_2 ((H - ( 1 / 3 )))
-    B = ((255 * temp_val))
+    temp_val = hue_to_rgb $var_1 $var_2 $((H - ( 1 / 3 )))
+    B = $((255 * temp_val))
   fi
 
   return printf "%.2x%.2x%.2x" $R $G $B
