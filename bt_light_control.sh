9129677cf4b9a202ae37efebd0a6d213a7fe55cd
jcoutch <joseph.coutcher@gmail.com>
2022-03-12 23:12:00 -0500

    Don't need state saving anymore

--- bt_light_control.sh
+++ bt_light_control.sh
@@ -18,13 +18,9 @@ SCRIPT_PATH="$(dirname $BASH_SOURCE)"
 mac=$1
 state=$2
 
-current_state=`cat ./current-state.json`
-
-if test "$current_state"; then
-  hue=`echo $current_state|jq ".H"`
-  sat=`echo $current_state|jq ".S"`
-  level=`echo $current_state|jq ".L"`
-fi
+hue=$3
+sat=$4
+level=$5
 
 if test -z "$hue"; then hue="0"; fi
 if test -z "$sat"; then sat="0"; fi
@@ -40,21 +36,9 @@ case $state in
     ;;
   
   color)
-    hue=$3
-    sat=$4
-
-    rgb_color=`$SCRIPT_PATH/hsl_to_rgb.sh "$hue" "$sat" "$level"`
-    gatttool -i $bt_controller -b $mac --char-write-req -a 0x0009 -n "56${rgb_color}00f0aa"
-    ;;
-  
-  level)
-    level=$3
-
     rgb_color=`$SCRIPT_PATH/hsl_to_rgb.sh "$hue" "$sat" "$level"`
     gatttool -i $bt_controller -b $mac --char-write-req -a 0x0009 -n "56${rgb_color}00f0aa"
     ;;
 esac
 
-echo "{ \"H\": $hue, \"S\": $sat, \"L\": $level }" > current-state.json
-
-echo "Light Color: $rgb_color, State: $state, Level: $level, Hue: $hue, Sat: $sat, Script Path: $SCRIPT_PATH"
+# echo "Light Color: $rgb_color, State: $state, Level: $level, Hue: $hue, Sat: $sat, Script Path: $SCRIPT_PATH"
